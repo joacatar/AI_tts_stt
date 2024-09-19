7955941f45531eadb7c66b9d4fa6ace8b57e1fbe
joacatar <143612564+joacatar@users.noreply.github.com>
2024-09-18 22:37:23 -0400

    First commit for project 1

---
+++ app.py
@@ -0,0 +1,91 @@
+from datetime import datetime
+
+from flask import Flask, render_template, request, redirect, url_for, send_file, send_from_directory
+from werkzeug.utils import secure_filename
+
+import os
+
+app = Flask(__name__)
+
+# Configure upload folder
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'wav'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def get_files():
+    files = []
+    for filename in os.listdir(UPLOAD_FOLDER):
+        if allowed_file(filename):
+            files.append(filename)
+            print(filename)
+    files.sort(reverse=True)
+    return files
+
+
+@app.route('/')
+def index():
+    files = get_files()
+    return render_template('index.html', files=files)
+
+
+@app.route('/upload', methods=['POST'])
+def upload_audio():
+    if 'audio_data' not in request.files:
+        flash('No audio data')
+        return redirect(request.url)
+    file = request.files['audio_data']
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file:
+        # filename = secure_filename(file.filename)
+        filename = datetime.now().strftime("%Y%m%d-%I%M%S%p") + '.wav'
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+
+        #
+        #
+        # Modify this block to call the speech to text API
+        # Save transcript to same filename but .txt
+        #
+        #
+
+    return redirect('/') #success
+
+
+@app.route('/upload/<filename>')
+def get_file(filename):
+    return send_file(filename)
+
+    
+@app.route('/upload_text', methods=['POST'])
+def upload_text():
+    text = request.form['text']
+    print(text)
+    #
+    #
+    # Modify this block to call the stext to speech API
+    # Save the output as a audio file in the 'tts' directory 
+    # Display the audio files at the bottom and allow the user to listen to them
+    #
+
+    return redirect('/') #success
+
+@app.route('/script.js',methods=['GET'])
+def scripts_js():
+    return send_file('./script.js')
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
